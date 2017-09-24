63d041e2c7281b32da23b9e11202475cd5075fd5
NuclearCoder <hd85.ms@gmail.com>
2017-09-24 22:49:42 +0200

    Added proper REPL class

---
+++ src/brainfuck/BrainfuckREPL.kt
@@ -0,0 +1,34 @@
+package brainfuck
+
+import brainfuck.parser.Interpreter
+import brainfuck.runtime.Context
+import brainfuck.runtime.Input
+import brainfuck.runtime.Output
+
+object BrainfuckREPL {
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val sb = StringBuilder()
+
+        val context = Context {
+            input = Input.SYSTEM
+            output = Output.of(sb)
+        }
+
+        while (true) {
+            print("brainfuck> ")
+            val input = readLine()
+            if (input == null || input.isBlank()) return
+            try {
+                sb.setLength(0)
+                Interpreter.run(context, input)
+                println(sb)
+                println()
+            } catch (e: Exception) {
+                System.err.println("Exception thrown: " + e.message)
+            }
+        }
+    }
+
+}
