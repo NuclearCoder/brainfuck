e7f43d080f979ab1d6c100f0401913890375b69a
NuclearCoder <hd85.ms@gmail.com>
2017-09-24 18:32:54 +0200

    Added checks for proper loop placement

--- src/brainfuck/impl/parser/BFParser.kt
+++ src/brainfuck/impl/parser/BFParser.kt
@@ -1,18 +1,29 @@
 package brainfuck.impl.parser
 
-import brainfuck.instructions.*
+import brainfuck.instructions.Decrement
+import brainfuck.instructions.Increment
+import brainfuck.instructions.InstructionQueue
+import brainfuck.instructions.Loop
+import brainfuck.instructions.Read
+import brainfuck.instructions.ShiftLeft
+import brainfuck.instructions.ShiftRight
+import brainfuck.instructions.Write
 import brainfuck.parser.Parser
 import brainfuck.parser.Token
 import brainfuck.parser.TokenQueue
 
 internal object BFParser : Parser {
 
-    override fun parse(tokens: TokenQueue): InstructionQueue = parse(tokens, isLoop = false)
+    override fun parse(tokens: TokenQueue): InstructionQueue = parse(tokens, depth = 0)
 
-    private fun parse(tokens: TokenQueue, isLoop: Boolean = false): InstructionQueue = InstructionQueue().also {
+    private fun parse(tokens: TokenQueue, depth: Int): InstructionQueue = InstructionQueue().also {
         while (tokens.isNotEmpty()) {
             val token = tokens.remove()
-            if (token == Token.END_LOOP && isLoop) return@also
+            if (token == Token.END_LOOP) {
+                if (depth <= 0)
+                    throw IllegalStateException("Closed inexistent loop")
+                return@also
+            }
             val instruction = when (token) {
                 Token.INCR -> Increment
                 Token.DECR -> Decrement
@@ -20,11 +31,13 @@ internal object BFParser : Parser {
                 Token.RIGHT -> ShiftRight
                 Token.READ -> Read
                 Token.WRITE -> Write
-                Token.START_LOOP -> Loop(parse(tokens, isLoop = true))
+                Token.START_LOOP -> Loop(parse(tokens, depth + 1))
                 else -> null
             } ?: continue
             it.add(instruction)
         }
+        if (depth > 0)
+            throw IllegalStateException("Unclosed loop")
     }
 
 }
